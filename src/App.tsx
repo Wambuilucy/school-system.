29c1839edefe84d8b287624170e1bd9971eb2f58
wambuilucy <wambuilucie99@gmail.com>
2026-03-05 14:49:03 +0300

    task done

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
+import TeacherDashboard from "./pages/TeacherDashboard";
+import ParentDashboard from "./pages/ParentDashboard";
+import StudentsDashboard from "./pages/StudentsDashboard";
 import Compose from "./pages/Compose";
 import Groups from "./pages/Groups";
 import Templates from "./pages/Templates";
 import History from "./pages/History";
 import Settings from "./pages/Settings";
-import TeacherDashboard from "./pages/TeacherDashboard";
-import ParentDashboard from "./pages/ParentDashboard";
-import StudentsDashboard from "./pages/StudentsDashboard";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -24,14 +24,14 @@ const App = () => (
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          <Route path="/teacher" element={<TeacherDashboard />} />
+          <Route path="/parent" element={<ParentDashboard />} />
+          <Route path="/students" element={<StudentsDashboard />} />
           <Route path="/compose" element={<Compose />} />
           <Route path="/groups" element={<Groups />} />
           <Route path="/templates" element={<Templates />} />
           <Route path="/history" element={<History />} />
           <Route path="/settings" element={<Settings />} />
-          <Route path="/teacher" element={<TeacherDashboard />} />
-          <Route path="/parent" element={<ParentDashboard />} />
-          <Route path="/students" element={<StudentsDashboard />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
